e0410d5a4c7523fad4ee141f669f96a0c114984f
aclason <ajclason@gmail.com>
2022-12-07 19:58:24 -0800

    Some tidying up, and clarifying objectives for the repo

--- R/Functions.R
+++ R/Functions.R
@@ -3,8 +3,8 @@
 #
 #' Import PSP data
 #'
-#' @param r.path pathway to data
-#' @param dat.type samples or trees?
+#' @param data_path pathway to data
+#' @param data_type samples or trees?
 #' @param tsas which tsas to include (vector of characters I think)
 #'
 #' @details Import tree and samples data
@@ -12,13 +12,13 @@
 #' @export
 #'
 #' @examples
-import.psp <- function(r.path, dat.type, tsas){
+importPSP <- function(data_path, data_type, tsas){
   read.list <- list()
   dat.list <- list()
-  for(i in 1:length(dat.type)){
+  for(i in 1:length(data_type)){
     #setwd(paste0(r.path,dat.type[i]))
     for(j in 1:length(tsas)){
-      read.list[[j]]<- fread(paste0(r.path,dat.type[i],"/","TSA",tsas[j],".csv"))
+      read.list[[j]]<- fread(paste0(data_path,data_type[i],"/","TSA",tsas[j],".csv"))
     }
     dat.list[[i]] <- rbindlist(read.list)
   }
